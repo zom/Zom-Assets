582320fa9fc427c249f1ced1ee02e0405489fef3
EironRabbit <iamironrabbit@gmail.com>
2019-02-01 11:54:13 -0500

    fix resize script

--- stickers/resize.sh
+++ stickers/resize.sh
@@ -3,10 +3,11 @@
 #first resize to make small
 mogrify -path "$2" -resize 320x320 "$1"
 
-#then reduce collers
-pngquant 64 "$2"
+#then reduce colors
+pngquant 64 "$2/$1"
 
 #then rename
+rm "$2/$1"
 rename -v 's/-fs8//' $2/*.png
 rename -v 'y/A-Z/a-z/' $2/*.png
 
